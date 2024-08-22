92e53fe373c0e796de8c8e7a28592b32654e24eb
José Vadell <me@jlvadell.com>
2024-08-22 17:30:12 +0200

    update deploy script

--- scripts/build_prod_env.sh
+++ scripts/build_prod_env.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 # create google creds file
 echo $SPREADSHEET_CREDS > "google_creds.json"
 # create .env.production file
-echo $DISCORD_TOKEN > "env.production"
-echo $GS_ATTENDANCE_PLAYER_MAPPING >> "env.production"
-echo $GUILD_ID >> "env.production"
-echo $PLAYERS >> "env.production"
+echo "DISCORD_TOKEN="$DISCORD_TOKEN > ".env.production"
+echo "GS_ATTENDANCE_PLAYER_MAPPING="$GS_ATTENDANCE_PLAYER_MAPPING >> ".env.production"
+echo "GUILD_ID="$GUILD_ID >> ".env.production"
+echo "PLAYERS="$PLAYERS >> ".env.production"
