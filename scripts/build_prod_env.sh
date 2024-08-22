b98dbd07215bf1b27bee51db836535a8c81e4696
José Vadell <me@jlvadell.com>
2024-08-22 17:14:08 +0200

    test change

--- scripts/build_prod_env.sh
+++ scripts/build_prod_env.sh
@@ -2,7 +2,7 @@
 # create google creds file
 echo $SPREADSHEET_CREDS > "google_creds.json"
 # create .env.production file
-echo $DISCORD_TOKEN > ".env.production"
-echo $GS_ATTENDANCE_PLAYER_MAPPING >> ".env.production"
-echo $GUILD_ID >> ".env.production"
-echo $PLAYERS >> ".env.production"
+echo $DISCORD_TOKEN > "env.production"
+echo $GS_ATTENDANCE_PLAYER_MAPPING >> "env.production"
+echo $GUILD_ID >> "env.production"
+echo $PLAYERS >> "env.production"
