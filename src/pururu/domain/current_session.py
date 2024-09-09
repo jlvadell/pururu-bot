9d1fa71c6ad9d13c1393eb739cdf8d06715d7f5f
José Vadell <me@jlvadell.com>
2024-09-09 23:04:05 +0200

    extract currentGame to new Module (current_session)

---
+++ src/pururu/domain/current_session.py
@@ -0,0 +1,152 @@
+import pururu.utils as utils
+import pururu.config as config
+from datetime import datetime
+
+
+class CurrentSession:
+    def __init__(self):
+        self.online_players = set()
+        self.players_clock_ins = {}
+        self.players_clock_outs = {}
+        self.game_id = None
+        self.logger = self.logger = utils.get_logger(__name__)
+
+    def clock_in(self, player: str, time: str = None) -> None:
+        """
+        Clocks in a player
+        :param player: member.name
+        :param time: time of clock in
+        :return: None
+        """
+        if time is None:
+            time = utils.get_current_time_formatted()
+        self.online_players.add(player)
+        if player not in self.players_clock_ins:
+            self.players_clock_ins[player] = []
+            self.players_clock_outs[player] = []
+        if len(self.players_clock_ins[player]) == len(self.players_clock_outs[player]):
+            self.players_clock_ins[player].append(time)
+
+    def clock_out(self, player: str, time: str = None) -> None:
+        """
+        Clocks out a player
+        :param player: member.name
+        :param time: time of clock out
+        :return: None
+        """
+        if time is None:
+            time = utils.get_current_time_formatted()
+        if player not in self.online_players:
+            self.logger.error(f"Player {player} not found in online_players")
+            return
+        self.online_players.remove(player)
+        if player not in self.players_clock_outs:
+            self.logger.error(f"Player {player} not found in clock_outs")
+            return
+        self.players_clock_outs[player].append(time)
+
+    def get_player_time(self, player: str) -> int:
+        """
+        Gets the total playtime of a player in seconds.
+        :param player: member.name
+        :return: int: total playtime in seconds
+        """
+        if player not in self.players_clock_ins:
+            return 0
+        clock_ins = self.players_clock_ins[player]
+        clock_outs = self.players_clock_outs[player]
+        total_time = 0
+        for i in range(len(clock_ins)):
+            clock_in = datetime.strptime(clock_ins[i], utils.FORMATTED_TIME_STR)
+            clock_out = datetime.strptime(clock_outs[i], utils.FORMATTED_TIME_STR) if i < len(
+                clock_outs) else datetime.now()
+            total_time += (clock_out - clock_in).total_seconds()
+        return int(total_time)
+
+    def should_start_new_game(self) -> bool:
+        """
+        Checks if the conditions to start a new game are met
+        :return: bool
+        """
+        return self.game_id is None and len(self.online_players) >= config.MIN_ATTENDANCE_MEMBERS
+
+    def should_end_game(self) -> bool:
+        """
+        Checks if the conditions to end the current game are met
+        :return: bool
+        """
+        return self.game_id is not None and len(self.online_players) < config.MIN_ATTENDANCE_MEMBERS
+
+    def get_players(self) -> list:
+        """
+        Returns the list of the current players
+        :return: list
+        """
+        return list(self.online_players)
+
+    def __str__(self):
+        return f"Game {self.game_id}, players {self.online_players}, clock_ins {self.players_clock_ins}, " \
+               f"clock_outs {self.players_clock_outs}"
+
+    def reset(self):
+        """
+        Resets the current game to initial state
+        :return: None
+        """
+        self.__init__()
+
+    def adjust_players_clocking_start_time(self, start_time: datetime) -> None:
+        """
+        Ensures that no player has a clock_in or period before start_time
+        :param start_time: Game start time
+        :return: None
+        """
+        for player in self.players_clock_ins:
+            player_clock_ins = self.players_clock_ins[player]
+            player_clock_outs = self.players_clock_outs[player]
+            adjusted_clock_ins = []
+            adjusted_clock_outs = []
+            for i, clock_in in enumerate(player_clock_ins):
+                if len(player_clock_outs) > i:
+                    clock_out = player_clock_outs[i]
+                    clock_out_time = datetime.strptime(clock_out, utils.FORMATTED_TIME_STR)
+                    if clock_out_time <= start_time:
+                        self.logger.debug(f"Discarding clock_in {clock_in} and clock_out {clock_out} for "
+                                          f"player {player} because it is before start_time {start_time}")
+                        continue
+                    else:
+                        adjusted_clock_outs.append(clock_out)
+                clock_in_time = datetime.strptime(clock_in, utils.FORMATTED_TIME_STR)
+                clock_in = clock_in if clock_in_time > start_time else start_time.strftime(utils.FORMATTED_TIME_STR)
+                adjusted_clock_ins.append(clock_in)
+
+            self.players_clock_ins[player] = adjusted_clock_ins
+            self.players_clock_outs[player] = adjusted_clock_outs
+
+    def adjust_players_clocking_end_time(self, end_time: datetime) -> None:
+        """
+        Ensures that no player has a clock_out or period after end_time
+        :param end_time: Game end time
+        :return: None
+        """
+        for player in self.players_clock_outs:
+            player_clock_ins = self.players_clock_ins[player]
+            player_clock_outs = self.players_clock_outs[player]
+            adjusted_clock_ins = []
+            adjusted_clock_outs = []
+            for i, clock_in in enumerate(player_clock_ins):
+                clock_out = player_clock_outs[i] if len(player_clock_outs) > i else end_time.strftime(
+                    utils.FORMATTED_TIME_STR)
+                clock_in_time = datetime.strptime(clock_in, utils.FORMATTED_TIME_STR)
+                if clock_in_time >= end_time:
+                    self.logger.debug(f"Discarding clock_in {clock_in} and clock_out {clock_out} for "
+                                      f"player {player} because it is after end_time {end_time}")
+                    continue
+                else:
+                    adjusted_clock_ins.append(clock_in)
+                clock_out_time = datetime.strptime(clock_out, utils.FORMATTED_TIME_STR)
+                clock_out = clock_out if clock_out_time <= end_time else end_time.strftime(utils.FORMATTED_TIME_STR)
+                adjusted_clock_outs.append(clock_out)
+
+            self.players_clock_ins[player] = adjusted_clock_ins
+            self.players_clock_outs[player] = adjusted_clock_outs
---
+++ src/tests/test_domain/test_current_session.py
@@ -0,0 +1,340 @@
+from datetime import datetime
+from unittest.mock import patch
+
+from freezegun import freeze_time
+from hamcrest import assert_that, equal_to, has_items
+
+from pururu.domain.current_session import CurrentSession
+
+
+@patch("pururu.utils.get_current_time_formatted", return_value="2023-08-10")
+def test_clock_in_ok_use_default_time(utils_mock):
+    # Given
+    current_session = CurrentSession()
+    # When
+    current_session.clock_in("member1")
+    # Then
+    assert_that(current_session.online_players, equal_to({"member1"}))
+    assert_that(current_session.players_clock_ins, equal_to({"member1": ["2023-08-10"]}))
+    assert_that(current_session.players_clock_outs, equal_to({"member1": []}))
+
+
+def test_clock_in_ok_use_given_time():
+    # Given
+    current_session = CurrentSession()
+    # When
+    current_session.clock_in("member1", "given_time")
+    # Then
+    assert_that(current_session.online_players, equal_to({"member1"}))
+    assert_that(current_session.players_clock_ins, equal_to({"member1": ["given_time"]}))
+    assert_that(current_session.players_clock_outs, equal_to({"member1": []}))
+
+
+@patch("pururu.utils.get_current_time_formatted", return_value="2023-08-10 10:00:00")
+def test_clock_in_second_clock_in(utils_mock):
+    # Given
+    current_session = CurrentSession()
+    current_session.players_clock_ins = {"member1": ["2023-08-10 09:00:00"]}
+    current_session.players_clock_outs = {"member1": ["2023-08-10 09:45:00"]}
+    # When
+    current_session.clock_in("member1")
+    # Then
+    assert_that(current_session.online_players, equal_to({"member1"}))
+    assert_that(current_session.players_clock_ins,
+                equal_to({"member1": ["2023-08-10 09:00:00", "2023-08-10 10:00:00"]}))
+    assert_that(current_session.players_clock_outs, equal_to({"member1": ["2023-08-10 09:45:00"]}))
+
+
+@patch("pururu.utils.get_current_time_formatted", return_value="2023-08-10 10:00:00")
+def test_clock_in_already_online(utils_mock):
+    # Given
+    current_session = CurrentSession()
+    current_session.online_players = {"member1"}
+    current_session.players_clock_ins = {"member1": ["2023-08-10 09:00:00"]}
+    current_session.players_clock_outs = {"member1": []}
+    # When
+    current_session.clock_in("member1")
+    # Then
+    assert_that(current_session.online_players, equal_to({"member1"}))
+    assert_that(current_session.players_clock_ins, equal_to({"member1": ["2023-08-10 09:00:00"]}))
+    assert_that(current_session.players_clock_outs, equal_to({"member1": []}))
+
+
+@patch("pururu.utils.get_current_time_formatted", return_value="2023-08-10 10:00:00")
+def test_clock_out_ok_use_default_time(utils_mock):
+    # Given
+    current_session = CurrentSession()
+    current_session.online_players = {"member1"}
+    current_session.players_clock_ins = {"member1": ["2023-08-10 09:00:00"]}
+    current_session.players_clock_outs = {"member1": []}
+    # When
+    current_session.clock_out("member1")
+    # Then
+    assert_that(current_session.online_players, equal_to(set()))
+    assert_that(current_session.players_clock_ins, equal_to({"member1": ["2023-08-10 09:00:00"]}))
+    assert_that(current_session.players_clock_outs, equal_to({"member1": ["2023-08-10 10:00:00"]}))
+
+
+def test_clock_out_ok_use_given_time():
+    # Given
+    current_session = CurrentSession()
+    current_session.online_players = {"member1"}
+    current_session.players_clock_ins = {"member1": ["2023-08-10 09:00:00"]}
+    current_session.players_clock_outs = {"member1": []}
+    # When
+    current_session.clock_out("member1", "given_time")
+    # Then
+    assert_that(current_session.online_players, equal_to(set()))
+    assert_that(current_session.players_clock_ins, equal_to({"member1": ["2023-08-10 09:00:00"]}))
+    assert_that(current_session.players_clock_outs, equal_to({"member1": ["given_time"]}))
+
+
+def test_clock_out_player_not_online():
+    # Given
+    current_session = CurrentSession()
+    current_session.players_clock_ins = {"member1": ["2023-08-10 09:00:00"]}
+    current_session.players_clock_outs = {"member1": ["2023-08-10 09:45:00"]}
+    # When
+    current_session.clock_out("member1")
+    # Then
+    assert_that(current_session.online_players, equal_to(set()))
+    assert_that(current_session.players_clock_ins, equal_to({"member1": ["2023-08-10 09:00:00"]}))
+    assert_that(current_session.players_clock_outs, equal_to({"member1": ["2023-08-10 09:45:00"]}))
+
+
+def test_clock_out_player_not_in_clock_out():
+    # Given
+    current_session = CurrentSession()
+    current_session.online_players = {"member1"}
+    current_session.players_clock_ins = {}
+    current_session.players_clock_outs = {}
+    # When
+    current_session.clock_out("member1")
+    # Then
+    assert_that(current_session.online_players, equal_to(set()))
+    assert_that(current_session.players_clock_ins, equal_to({}))
+    assert_that(current_session.players_clock_outs, equal_to({}))
+
+
+def test_get_player_time_ok():
+    # Given
+    current_session = CurrentSession()
+    current_session.players_clock_ins = {"member1": ["2023-08-10 09:00:00", "2023-08-10 09:30:00"]}
+    current_session.players_clock_outs = {"member1": ["2023-08-10 09:15:00", "2023-08-10 10:00:00"]}
+    expected_player_time = 2700  # 15 min + 30 min = 45 min = 2700 sec
+    # When
+    actual_player_time = current_session.get_player_time("member1")
+    # Then
+    assert_that(actual_player_time, equal_to(expected_player_time))
+
+
+@freeze_time("2023-08-10 10:00:00")
+def test_get_player_time_ok_while_still_online():
+    # Given
+    current_session = CurrentSession()
+    current_session.online_players = {"member1"}
+    current_session.players_clock_ins = {"member1": ["2023-08-10 09:30:00"]}
+    current_session.players_clock_outs = {"member1": []}
+    expected_player_time = 1800  # 30 min
+    # When
+    actual_player_time = current_session.get_player_time("member1")
+    # Then
+    assert_that(actual_player_time, equal_to(expected_player_time))
+
+
+def test_get_player_time_no_player_data():
+    # Given
+    current_session = CurrentSession()
+    expected_player_time = 0
+    # When
+    actual_player_time = current_session.get_player_time("member1")
+    # Then
+    assert_that(actual_player_time, equal_to(expected_player_time))
+
+
+@patch("pururu.config.MIN_ATTENDANCE_MEMBERS", 2)
+def test_should_start_new_game_true():
+    # Given
+    current_session = CurrentSession()
+    current_session.online_players = {"member1", "member2"}
+    current_session.game_id = None
+    # When
+    actual_should_start_new_game = current_session.should_start_new_game()
+    # Then
+    assert_that(actual_should_start_new_game, equal_to(True))
+
+
+@patch("pururu.config.MIN_ATTENDANCE_MEMBERS", 2)
+def test_should_start_new_game_false_game_already_going():
+    # Given
+    current_session = CurrentSession()
+    current_session.online_players = {"member1", "member2"}
+    current_session.game_id = 1
+    # When
+    actual_should_start_new_game = current_session.should_start_new_game()
+    # Then
+    assert_that(actual_should_start_new_game, equal_to(False))
+
+
+@patch("pururu.config.MIN_ATTENDANCE_MEMBERS", 3)
+def test_should_start_new_game_false_not_enough_players():
+    # Given
+    current_session = CurrentSession()
+    current_session.online_players = {"member1", "member2"}
+    current_session.game_id = None
+    # When
+    actual_should_start_new_game = current_session.should_start_new_game()
+    # Then
+    assert_that(actual_should_start_new_game, equal_to(False))
+
+
+@patch("pururu.config.MIN_ATTENDANCE_MEMBERS", 3)
+def test_should_end_game_true():
+    # Given
+    current_session = CurrentSession()
+    current_session.online_players = {"member1", "member2"}
+    current_session.game_id = 1
+    # When
+    actual_should_end_game = current_session.should_end_game()
+    # Then
+    assert_that(actual_should_end_game, equal_to(True))
+
+
+@patch("pururu.config.MIN_ATTENDANCE_MEMBERS", 3)
+def test_should_end_game_false_theres_no_game():
+    # Given
+    current_session = CurrentSession()
+    current_session.online_players = {"member1", "member2"}
+    current_session.game_id = None
+    # When
+    actual_should_end_game = current_session.should_end_game()
+    # Then
+    assert_that(actual_should_end_game, equal_to(False))
+
+
+@patch("pururu.config.MIN_ATTENDANCE_MEMBERS", 1)
+def test_should_end_game_false_too_many_players():
+    # Given
+    current_session = CurrentSession()
+    current_session.online_players = {"member1", "member2"}
+    current_session.game_id = 1
+    # When
+    actual_should_end_game = current_session.should_end_game()
+    # Then
+    assert_that(actual_should_end_game, equal_to(False))
+
+
+def test_get_players():
+    # Given
+    current_session = CurrentSession()
+    current_session.online_players = {"member1", "member2"}
+    # When
+    actual_players = current_session.get_players()
+    # Then
+    assert_that(actual_players, has_items("member1", "member2"))
+
+
+def test_str():
+    # Given
+    current_session = CurrentSession()
+    current_session.game_id = 1
+    current_session.online_players = {"member1"}
+    current_session.players_clock_ins = {"member1": ["2023-08-10 09:00:00"]}
+    current_session.players_clock_outs = {"member1": ["2023-08-10 09:45:00"]}
+    expected_str = ("Game 1, players {'member1'}, clock_ins {'member1': ['2023-08-10 09:00:00']}, "
+                    "clock_outs {'member1': ['2023-08-10 09:45:00']}")
+    # When
+    actual_str = str(current_session)
+    # Then
+    assert_that(actual_str, equal_to(expected_str))
+
+
+def test_reset():
+    # Given
+    current_session = CurrentSession()
+    current_session.game_id = 1
+    current_session.online_players = {"member1"}
+    current_session.players_clock_ins = {"member1": ["2023-08-10 09:00:00"]}
+    current_session.players_clock_outs = {"member1": ["2023-08-10 09:45:00"]}
+    # When
+    current_session.reset()
+    # Then
+    assert_that(current_session.game_id, equal_to(None))
+    assert_that(current_session.online_players, equal_to(set()))
+    assert_that(current_session.players_clock_ins, equal_to({}))
+    assert_that(current_session.players_clock_outs, equal_to({}))
+
+
+def test_adjust_players_clocking_start_time_period_fully_in_the_past():
+    # Given
+    current_session = CurrentSession()
+    current_session.players_clock_ins = {"member1": ["2023-08-10 08:30:00"]}
+    current_session.players_clock_outs = {"member1": ["2023-08-10 08:40:00"]}
+    # When
+    current_session.adjust_players_clocking_start_time(start_time=datetime(2023, 8, 10, 9))
+    # Then
+    assert_that(current_session.players_clock_ins, equal_to({"member1": []}))
+    assert_that(current_session.players_clock_outs, equal_to({"member1": []}))
+
+
+def test_adjust_players_clocking_start_time_period_partial_in_the_past():
+    # Given
+    current_session = CurrentSession()
+    current_session.players_clock_ins = {"member1": ["2023-08-10 08:30:00"]}
+    current_session.players_clock_outs = {"member1": ["2023-08-10 09:45:00"]}
+    # When
+    current_session.adjust_players_clocking_start_time(start_time=datetime(2023, 8, 10, 9))
+    # Then
+    assert_that(current_session.players_clock_ins, equal_to({"member1": ["2023-08-10 09:00:00"]}))
+    assert_that(current_session.players_clock_outs, equal_to({"member1": ["2023-08-10 09:45:00"]}))
+
+
+def test_adjust_players_clocking_start_time_period_in_range():
+    # Given
+    current_session = CurrentSession()
+    current_session.players_clock_ins = {"member1": ["2023-08-10 09:00:00", "2023-08-10 09:50:00"]}
+    current_session.players_clock_outs = {"member1": ["2023-08-10 09:45:00"]}
+    # When
+    current_session.adjust_players_clocking_start_time(start_time=datetime(2023, 8, 10, 9))
+    # Then
+    assert_that(current_session.players_clock_ins,
+                equal_to({"member1": ["2023-08-10 09:00:00", "2023-08-10 09:50:00"]}))
+    assert_that(current_session.players_clock_outs, equal_to({"member1": ["2023-08-10 09:45:00"]}))
+
+
+def test_adjust_players_clocking_end_time_period_fully_in_the_future():
+    # Given
+    current_session = CurrentSession()
+    current_session.players_clock_ins = {"member1": ["2023-08-10 09:30:00"]}
+    current_session.players_clock_outs = {"member1": ["2023-08-10 09:45:00"]}
+    # When
+    current_session.adjust_players_clocking_end_time(end_time=datetime(2023, 8, 10, 9))
+    # Then
+    assert_that(current_session.players_clock_ins, equal_to({"member1": []}))
+    assert_that(current_session.players_clock_outs, equal_to({"member1": []}))
+
+
+def test_adjust_players_clocking_end_time_period_partial_in_the_future():
+    # Given
+    current_session = CurrentSession()
+    current_session.players_clock_ins = {"member1": ["2023-08-10 08:30:00"]}
+    current_session.players_clock_outs = {"member1": ["2023-08-10 09:45:00"]}
+    # When
+    current_session.adjust_players_clocking_end_time(end_time=datetime(2023, 8, 10, 9))
+    # Then
+    assert_that(current_session.players_clock_ins, equal_to({"member1": ["2023-08-10 08:30:00"]}))
+    assert_that(current_session.players_clock_outs, equal_to({"member1": ["2023-08-10 09:00:00"]}))
+
+
+def test_adjust_players_clocking_end_time_period_in_range():
+    # Given
+    current_session = CurrentSession()
+    current_session.players_clock_ins = {"member1": ["2023-08-10 08:00:00", "2023-08-10 08:50:00"]}
+    current_session.players_clock_outs = {"member1": ["2023-08-10 08:45:00"]}
+    # When
+    current_session.adjust_players_clocking_end_time(end_time=datetime(2023, 8, 10, 9))
+    # Then
+    assert_that(current_session.players_clock_ins,
+                equal_to({"member1": ["2023-08-10 08:00:00", "2023-08-10 08:50:00"]}))
+    assert_that(current_session.players_clock_outs,
+                equal_to({"member1": ["2023-08-10 08:45:00", "2023-08-10 09:00:00"]}))
