eeb02adaa1e1f2c843dcc55c2183ee935df1fbdf
José Vadell <me@jlvadell.com>
2024-10-01 14:03:15 +0200

    pururu service &  handler poll event flow

--- src/pururu/application/services/pururu_handler.py
+++ src/pururu/application/services/pururu_handler.py
@@ -2,8 +2,9 @@ from datetime import datetime
 
 import pururu.config as config
 import pururu.utils as utils
-from pururu.application.events.entities import EndGameIntentEvent, GameStartedEvent, PururuEvent, GameEndedEvent
-from pururu.application.events.entities import MemberJoinedChannelEvent, MemberLeftChannelEvent, NewGameIntentEvent
+from pururu.application.events.entities import (EndGameIntentEvent, GameStartedEvent, PururuEvent, GameEndedEvent,
+                                                MemberJoinedChannelEvent, MemberLeftChannelEvent, NewGameIntentEvent,
+                                                CheckExpiredPollsEvent, FinalizePollEvent)
 from pururu.application.events.event_system import EventSystem
 from pururu.domain.entities import MemberStats
 from pururu.domain.exceptions import CannotStartNewGame, CannotEndGame, GameEndedWithoutPrecondition
@@ -15,6 +16,10 @@ class PururuHandler:
         self.domain_service = domain_service
         self.event_system = event_system
         self.logger = utils.get_logger(__name__)
+
+    # ---------------------------
+    # DISCORD EVENT HANDLERS
+    # ---------------------------
 
     def handle_voice_state_update_dc_event(self, member: str, before_channel: str | None,
                                            after_channel: str | None) -> None:
@@ -36,6 +41,23 @@ class PururuHandler:
             event = MemberLeftChannelEvent(member, before_channel, datetime.now())
         if event:
             self.__emit_event(event)
+
+    # ---------------------------
+    # DISCORD COMMAND HANDLERS
+    # ---------------------------
+
+    def retrieve_player_stats(self, player: str) -> MemberStats:
+        """
+        Retrieves the attendance stats of a player
+        :param player: player name
+        :return: MemberStats
+        """
+        self.logger.info(f"Retrieving stats for player {player}")
+        return self.domain_service.calculate_player_stats(player)
+
+    # ---------------------------
+    # PURURU EVENT HANDLERS
+    # ---------------------------
 
     def handle_member_joined_channel_event(self, event: MemberJoinedChannelEvent) -> None:
         """
@@ -112,15 +134,30 @@ class PururuHandler:
         """
         self.logger.info(f"Game {event.attendance.game_id} has ended with attendance {event.attendance}")
 
-    def retrieve_player_stats(self, player: str) -> MemberStats:
+    async def handle_check_expired_polls_event(self, event: CheckExpiredPollsEvent) -> None:
         """
-        Retrieves the attendance stats of a player
-        :param player: player name
-        :return: MemberStats
+        Handles the CheckExpiredPollsEvent
+        :param event: PururuEvent
+        :return: None
         """
-        self.logger.info(f"Retrieving stats for player {player}")
-        return self.domain_service.calculate_player_stats(player)
+        self.logger.info(f"Consuming: {event}")
+        expired_polls = await self.domain_service.get_expired_polls()
+        for poll in expired_polls:
+            self.__emit_event(FinalizePollEvent(poll))
 
+    async def handle_finalize_poll_event(self, event: FinalizePollEvent) -> None:
+        """
+        Handles the FinalizePollEvent
+        :param event: PururuEvent
+        :return: None
+        """
+        self.logger.info(f"Consuming: {event}")
+        await self.domain_service.finalize_poll(event.poll)
+        self.logger.debug(f"Ended poll {event.poll.message_id} with winners {event.poll.get_winners()}")
+
+    # ---------------------------
+    # PRIVATE METHODS
+    # ---------------------------
     def __emit_event(self, event: PururuEvent, delay: int = None) -> None:
         if delay:
             self.event_system.emit_event_with_delay(event, delay)
