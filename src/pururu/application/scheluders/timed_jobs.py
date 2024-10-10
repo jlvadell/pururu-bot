0d4162ef604dabe48c30166415344dbe329fdc9c
José Vadell <me@jlvadell.com>
2024-10-10 10:49:11 +0200

    add timed jobs

--- src/pururu/application/scheluders/timed_jobs.py
+++ src/pururu/application/scheluders/timed_jobs.py
@@ -1,8 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from pururu.application.services.pururu_handler import PururuHandler
-
 import pururu.utils as utils
+from pururu.application.services.pururu_handler import PururuHandler
 
 
 class ScheduledJobs:
@@ -11,10 +10,10 @@ class ScheduledJobs:
         self.pururu_handler = pururu_handler
         self.logger = utils.get_logger(__name__)
 
-        #------------------------------------
+        # ------------------------------------
         # Scheduled tasks
-        #------------------------------------
-        #self.scheduler.add_job(self.check_expired_polls_task, 'interval', minutes=1)
+        # ------------------------------------
+        self.scheduler.add_job(self.check_expired_polls_task, 'interval', hours=2)
 
     def start(self) -> None:
         """
@@ -24,11 +23,10 @@ class ScheduledJobs:
         self.logger.info("Starting scheduler")
         self.scheduler.start()
 
-
     def check_expired_polls_task(self) -> None:
         """
         Emits the CHECK_EXPIRED_POLLS event
         :return: None
         """
-        self.logger.info("Checking expired polls")
+        self.logger.debug("Checking expired polls task")
         self.pururu_handler.trigger_check_expired_polls_flow()
