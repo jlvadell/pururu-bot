ef1100783ccf30b04b19b4f770c0f9d961518a83
José Vadell <me@jlvadell.com>
2024-10-27 02:14:42 +0200

    implement circuit_breaker in google sheet adapter

--- src/tests/test_infrastructure/test_adapters/test_google_sheets/test_google_sheets_adapter.py
+++ src/tests/test_infrastructure/test_adapters/test_google_sheets/test_google_sheets_adapter.py
@@ -1,5 +1,5 @@
 import pytest
-from hamcrest import assert_that, has_length, equal_to
+from hamcrest import assert_that, has_length, equal_to, raises
 
 from pururu.domain.entities import Attendance, Clocking, BotEvent
 from pururu.infrastructure.adapters.google_sheets.entities import AttendanceSheet, ClockingSheet, BotEventSheet, \
@@ -15,11 +15,15 @@ from unittest.mock import patch, Mock, MagicMock
 @patch('pururu.infrastructure.adapters.google_sheets.google_sheets_adapter.gspread')
 @patch('google.oauth2.service_account.Credentials.from_service_account_file')
 @patch('pururu.common.utils.get_logger')
-def set_up(gspread_mock, credentials_mock, logger_mock) -> GoogleSheetsAdapter:
+@patch('pururu.common.circuit_breaker.CircuitBreaker')
+def set_up(gspread_mock, credentials_mock, logger_mock, circuit_breaker_mock) -> GoogleSheetsAdapter:
     gs_client_mock = Mock()
     gspread_mock.authorize.return_value = gs_client_mock
     gs_client_mock.open_by_key.return_value = gs_client_mock
-    return GoogleSheetsAdapter('credentials.json', 'spreadsheet_id')
+    circuit_breaker_mock.call.side_effect = lambda x, *y, **z: x(*y, **z)
+    adapter = GoogleSheetsAdapter('credentials.json', 'spreadsheet_id')
+    adapter.circuit_breaker = circuit_breaker_mock
+    return adapter
 
 
 @patch('pururu.infrastructure.adapters.google_sheets.google_sheets_adapter.mapper')
@@ -36,6 +40,7 @@ def test_upsert_attendance_ok(mapper_mock, attendance: Attendance, attendance_sh
               f"{AttendanceSheet.DATA_COL_END}{attendance_sheet.game_id}",
         params=adapter.DEFAULT_PARAMS, body={"values": [attendance_sheet.to_row_values()]}
     )
+    adapter.circuit_breaker.call.assert_called()
 
 
 @patch('pururu.infrastructure.adapters.google_sheets.google_sheets_adapter.mapper')
@@ -55,6 +60,7 @@ def test_upsert_clocking_game_id_exists(mapper_mock, clocking: Clocking, clockin
     adapter.spreadsheet.values_update.assert_called_with(
         range=f"{ClockingSheet.SHEET}!{ClockingSheet.DATA_COL_INIT}{expected_idx}:{ClockingSheet.DATA_COL_END}{expected_idx}",
         params=adapter.DEFAULT_PARAMS, body={"values": [clocking_sheet.to_row_values()]})
+    adapter.circuit_breaker.call.assert_called()
 
 
 @patch('pururu.infrastructure.adapters.google_sheets.google_sheets_adapter.mapper')
@@ -74,6 +80,7 @@ def test_upsert_clocking_game_id_not_found(mapper_mock, clocking: Clocking, cloc
     adapter.spreadsheet.values_update.assert_called_with(
         range=f"{ClockingSheet.SHEET}!{ClockingSheet.DATA_COL_INIT}{expected_idx}:{ClockingSheet.DATA_COL_END}{expected_idx}",
         params=adapter.DEFAULT_PARAMS, body={"values": [clocking_sheet.to_row_values()]})
+    adapter.circuit_breaker.call.assert_called()
 
 
 @patch('pururu.infrastructure.adapters.google_sheets.google_sheets_adapter.mapper')
@@ -104,6 +111,7 @@ def test_insert_bot_event_ok(mapper_mock, bot_event: BotEvent, bot_event_sheet: 
     adapter.spreadsheet.values_update.assert_called_with(
         range=f"{BotEventSheet.SHEET}!{BotEventSheet.DATA_COL_INIT}1",
         params=adapter.DEFAULT_PARAMS, body={"values": [bot_event_sheet.to_row_values()]})
+    adapter.circuit_breaker.call.assert_called()
 
 
 @patch('pururu.infrastructure.adapters.google_sheets.google_sheets_adapter.mapper')
@@ -118,6 +126,7 @@ def test_get_last_attendance_ok(mapper_mock, attendance: Attendance, attendance_
     result = adapter.get_last_attendance()
     # Then
     assert_that(result, equal_to(attendance))
+    adapter.circuit_breaker.force_check.assert_called()
 
 
 @patch('pururu.infrastructure.adapters.google_sheets.google_sheets_adapter.mapper')
@@ -137,6 +146,7 @@ def test_get_all_attendances_ok(mapper_mock, attendance_sheet: AttendanceSheet):
     adapter.spreadsheet.values_get.assert_called_with(
         f"{AttendanceSheet.SHEET}!{AttendanceSheet.DATA_COL_INIT}{AttendanceSheet.DATA_ROW_INIT}"
         f":{AttendanceSheet.DATA_COL_END}2")
+    adapter.circuit_breaker.force_check.assert_called()
 
 
 def test_get_player_coins_ok():
@@ -152,3 +162,50 @@ def test_get_player_coins_ok():
     adapter.spreadsheet.values_get.assert_called_with(
         f"{CoinsSheet.SHEET}!{CoinsSheet.DATA_COL_INIT}{CoinsSheet.DATA_ROW_INIT}"
         f":{CoinsSheet.DATA_COL_END}{CoinsSheet.DATA_ROW_END}")
+    adapter.circuit_breaker.force_check.assert_called()
+
+def test_fallback_in_memory_data_add():
+    # Given
+    adapter = set_up()
+    func = Mock(side_effect=lambda x, y: x+y)
+    # When
+    adapter.in_memory_fallback.add(func, 1, 2)
+    # Then
+    assert_that(adapter.in_memory_fallback.data, equal_to([{"function": func, "args":(1,2), "kwargs":{}}]))
+
+def test_fallback_in_memory_data_get_and_clear():
+    # Given
+    adapter = set_up()
+    func = Mock(side_effect=lambda x, y: x+y)
+    adapter.in_memory_fallback.data = [{"function": func, "args":(1,2), "kwargs":{}}, {"function": func, "args":(3,4), "kwargs":{}}, {"function": func, "args":(5,6), "kwargs":{}}]
+    # When
+    for idx, item in enumerate(adapter.in_memory_fallback.get_and_clear()):
+        item["function"](*item["args"], **item["kwargs"])
+    # Then
+    assert_that(adapter.in_memory_fallback.data, equal_to([]))
+
+def test_use_fallback_ok():
+    # Given
+    adapter = set_up()
+    func = Mock(side_effect=lambda x, y: x+y)
+    # When
+    adapter._use_fallback(func,1, 2)
+    # Then
+    assert_that(adapter.in_memory_fallback.data, equal_to([{"function": func, "args":(1,2), "kwargs":{}}]))
+
+def test_fallback_recovery_ok():
+    # Given
+    adapter = set_up()
+    func = Mock(side_effect=lambda x, y: x+y)
+    error_func = Mock(side_effect=Exception("Error"))
+    adapter.in_memory_fallback.data = [{"function": func, "args": (1, 2), "kwargs": {}},
+                                       {"function": error_func, "args": (3, 4), "kwargs": {}},
+                                       {"function": func, "args": (5, 6), "kwargs": {}}]
+    # When
+    with pytest.raises(Exception):
+        adapter._fallback_recovery()
+    # Then
+    assert_that(adapter.in_memory_fallback.data, equal_to([{"function": error_func, "args": (3, 4), "kwargs": {}},
+                                       {"function": func, "args": (5, 6), "kwargs": {}}]))
+    assert_that(func.call_count, equal_to(1))
+    assert_that(error_func.call_count, equal_to(1))
