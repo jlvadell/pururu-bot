3b1441c73b90c16b6a30b743084aed8d1025bc1c
José Vadell <me@jlvadell.com>
2025-06-19 16:33:35 +0200

    chore: add deprecation warning to circuit breaker

--- src/pururu/common/circuit_breaker.py
+++ src/pururu/common/circuit_breaker.py
@@ -1,7 +1,8 @@
 import time
+import warnings
 from enum import Enum
 
-from pururu.common import utils
+from pururu.common import logger
 from pururu.common.exceptions import CircuitBreakerException
 
 
@@ -12,15 +13,21 @@ class CircuitBreakerState(Enum):
 
 
 class CircuitBreaker:
+    """
+    DEPRECATION NOTICE: This class is deprecated and will be removed in a future version. with the current externalized events this is not needed anymore.
+    """
+
     def __init__(self, failure_threshold: int, recovery_timeout: int, open_fallback=None, on_half_open=None):
         self.failure_threshold = failure_threshold
         self.recovery_timeout = recovery_timeout
         self.failure_count = 0
         self.state = CircuitBreakerState.CLOSED
-        self.logger = utils.get_logger(__name__)
+        self.logger = logger.get_logger(__name__)
         self.last_failure_time = 0
         self.open_fallback = open_fallback
         self.on_half_open = on_half_open
+        warnings.warn("DEPRECATION NOTICE: CircuitBreaker is deprecated and will be removed in a future version.", DeprecationWarning)
+        self.logger.warning("CircuitBreaker is deprecated and will be removed in a future version. Please use the new event system instead.")
 
     def call(self, func, *args, **kwargs):
         if self.state == CircuitBreakerState.OPEN:
