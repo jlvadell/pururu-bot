0bfcf558449771aa70ed1e14f7e0c07e18390763
José Vadell <me@jlvadell.com>
2025-06-19 16:34:39 +0200

    test: remove usage of utils logger

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -8,15 +8,6 @@ from hamcrest import assert_that, equal_to
 from pururu.common import utils
 
 
-@patch("pururu.config.LOG_LEVEL", "DEBUG")
-def test_get_logger():
-    # Given-When
-    actual = utils.get_logger("name")
-    # Then
-    assert_that(actual.name, equal_to("name"))
-    assert_that(actual.getEffectiveLevel(), equal_to(logging.DEBUG))
-
-
 @freeze_time("2021-09-01 12:00:00")
 def test_get_current_time_formatted():
     # Given-When
