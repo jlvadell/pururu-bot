2fb956df52f5d67d58039432ea2b22aba972771d
José Vadell <me@jlvadell.com>
2025-06-19 16:36:20 +0200

    refactor: main, add graceful shutdown

--- src/pururu/bot.py
+++ src/pururu/bot.py
@@ -1,9 +1,11 @@
-import pururu.config as config
 import asyncio
-from pururu.common import utils
+import signal
+
+import pururu.config as config
 from pururu.application.events.event_consumers import GameEventConsumer, PollEventConsumer
 from pururu.application.scheluders.timed_jobs import ScheduledJobs
 from pururu.application.services.pururu_handler import PururuHandler
+from pururu.common import logger, utils
 from pururu.domain.services.pururu_service import PururuService
 from pururu.infrastructure.adapters.discord.discord_bot import PururuDiscordBot
 from pururu.infrastructure.adapters.discord.discord_service_adapter import DiscordServiceAdapter
@@ -23,9 +25,13 @@ class Application:
         self.scheduler = None
         self.game_event_consumer = None
         self.poll_event_consumer = None
-        self.logger = utils.get_logger(__name__)
+        self.tasks = []
+        self.logger = logger.get_logger(__name__)
 
     async def init(self):
+        self.logger.info(utils.get_banner())
+        self.logger.info("Initializing")
+
         # Event Service
         self.event_emitter_service = SNSEventServiceAdapter()
 
@@ -54,13 +60,51 @@ class Application:
         # Additional wiring
         self.pururu_service.set_discord_service(self.discord_service)
         self.scheduler.start()
-        asyncio.create_task(self.game_event_consumer.start_polling())
-        asyncio.create_task(self.poll_event_consumer.start_polling())
 
-        # Run Application
-        await self.discord_bot.start(config.DISCORD_TOKEN)
+        # start async processes
+        try:
+            async with asyncio.TaskGroup() as tg:
+                game_consumer = tg.create_task(self.game_event_consumer.start_polling(),
+                                               name="GameEventConsumer_polling")
+                poll_consumer = tg.create_task(self.poll_event_consumer.start_polling(),
+                                               name="PollEventConsumer_polling")
+                self.tasks.append(game_consumer)
+                self.tasks.append(poll_consumer)
+                await self.discord_bot.start(config.DISCORD_TOKEN)
+        except asyncio.CancelledError:
+            await self.shutdown()
+            raise
+
+    async def shutdown(self):
+        self.logger.info("Stopping application.......")
+        self.logger.info("Cancelling background tasks")
+        for task in self.tasks:
+            task.cancel()
+            try:
+                await task
+            except asyncio.CancelledError:
+                self.logger.debug(f"Task {task.get_name()} was cancelled successfully")
+        if self.scheduler:
+            self.scheduler.stop()
+            self.logger.info("Scheduler stopped")
+        await self.discord_bot.close()
+        self.logger.info("Discord connection closed")
+        self.logger.info("Application stopped")
 
 
 if __name__ == '__main__':
     app = Application()
-    asyncio.run(app.init())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    for sig in (signal.SIGINT, signal.SIGTERM):
+        try:
+            loop.add_signal_handler(sig, app.shutdown)
+        except NotImplementedError:
+            """Handle NotImplementedError on Windows"""
+            pass
+    try:
+        loop.run_until_complete(app.init())
+    except KeyboardInterrupt:
+        loop.run_until_complete(app.shutdown())
+    finally:
+        loop.close()
