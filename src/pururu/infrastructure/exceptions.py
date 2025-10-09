1388fa919fd5ebe7b87e7146a109cfa25c5275ee
José Vadell <me@jlvadell.com>
2025-10-09 22:04:41 +0200

    Adds Discord session info view message
    Implements functionality to send and edit session info views in Discord channels.
    
    Adds dedicated exceptions for Discord-related errors such as channel or message not found, and unexpected errors.
    This improves error handling and provides more informative logging.

--- src/pururu/infrastructure/exceptions.py
+++ src/pururu/infrastructure/exceptions.py
@@ -11,3 +11,16 @@ class SQSDeserializationException(InfrastructureException):
 class SQSHandlerNotFoundException(InfrastructureException):
     """Raised when no handler is found for a specific SQS event."""
     pass
+
+## Discord
+class DiscordChannelNotFoundException(InfrastructureException):
+    """Raised when a specified Discord channel is not found."""
+    pass
+
+class DiscordMessageNotFoundException(InfrastructureException):
+    """Raised when a specified Discord message is not found."""
+    pass
+
+class DiscordUnExpectedException(InfrastructureException):
+    """Raised when an unexpected error occurs in Discord operations."""
+    pass
