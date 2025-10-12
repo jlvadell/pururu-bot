15d7101074c51738448d9296674308f2421539df
José Vadell <me@jlvadell.com>
2025-10-12 17:25:58 +0200

    update redundant param

--- src/pururu/application/handlers/session_events_handler.py
+++ src/pururu/application/handlers/session_events_handler.py
@@ -123,8 +123,8 @@ class SessionEventsHandler:
 
     async def _update_session_info_view(self, session: Session) -> None:
         if settings.discord.enable_communication_channel:
-            channel_id = session.metadata.get(SessionMetadataKey.DISCORD_INFO_MESSAGE_CHANNEL_ID, None)
-            message_id = session.metadata.get(SessionMetadataKey.DISCORD_INFO_MESSAGE_ID, None)
+            channel_id = session.metadata.get(SessionMetadataKey.DISCORD_INFO_MESSAGE_CHANNEL_ID)
+            message_id = session.metadata.get(SessionMetadataKey.DISCORD_INFO_MESSAGE_ID)
             if channel_id and message_id:
                 await self.discord_service.update_session_info_view_message(channel_id, message_id, session)
 
