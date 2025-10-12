cc6ded20d4b7d28fc2918255c3a35b028b3a690f
José Vadell <me@jlvadell.com>
2025-10-12 16:33:06 +0200

    feat: auto justify when concluding an additional game

--- src/pururu/domain/entities/session.py
+++ src/pururu/domain/entities/session.py
@@ -262,7 +262,12 @@ class Session:
         official_start_time = self.get_official_start_time(min_players)
         official_end_time = self.get_official_end_time(min_players)
         for player in self.players:
-            player.attended = player.has_attended(min_playtime, official_start_time, official_end_time)
+            attended = player.has_attended(min_playtime, official_start_time, official_end_time)
+            player.attended = attended
+            if not attended and self.type == Type.ADDITIONAL_GAME:
+                player.justified_absence = True
+                player.motive = "Additional game absence"
+
         duration = int((official_end_time - official_start_time).total_seconds())
         attended_count = self.count_attended_players()
 
